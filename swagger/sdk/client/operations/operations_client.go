2979892b39d420408f0f2a7ba6e47143162f88d9
sj <sebastianthegreatful@gmail.com>
2022-01-18 13:50:49 +0700

    docs: document health endpoint

--- swagger/sdk/client/operations/operations_client.go
+++ swagger/sdk/client/operations/operations_client.go
@@ -38,6 +38,8 @@ type ClientService interface {
 
 	FindDatabaseURLByID(params *FindDatabaseURLByIDParams, opts ...ClientOption) (*FindDatabaseURLByIDOK, error)
 
+	Health(params *HealthParams, opts ...ClientOption) (*HealthOK, error)
+
 	ListDatabases(params *ListDatabasesParams, authInfo runtime.ClientAuthInfoWriter, opts ...ClientOption) (*ListDatabasesOK, error)
 
 	LockDatabaseByID(params *LockDatabaseByIDParams, authInfo runtime.ClientAuthInfoWriter, opts ...ClientOption) (*LockDatabaseByIDOK, error)
@@ -205,6 +207,44 @@ func (a *Client) FindDatabaseURLByID(params *FindDatabaseURLByIDParams, opts ...
 	// unexpected success response
 	// safeguard: normally, absent a default response, unknown success responses return an error above: so this is a codegen issue
 	msg := fmt.Sprintf("unexpected success response for findDatabaseUrlById: API contract not enforced by server. Client expected to get an error, but got: %T", result)
+	panic(msg)
+}
+
+/*
+  Health Service health status
+*/
+func (a *Client) Health(params *HealthParams, opts ...ClientOption) (*HealthOK, error) {
+	// TODO: Validate the params before sending
+	if params == nil {
+		params = NewHealthParams()
+	}
+	op := &runtime.ClientOperation{
+		ID:                 "health",
+		Method:             "GET",
+		PathPattern:        "/health",
+		ProducesMediaTypes: []string{"application/json"},
+		ConsumesMediaTypes: []string{"application/json"},
+		Schemes:            []string{"http"},
+		Params:             params,
+		Reader:             &HealthReader{formats: a.formats},
+		Context:            params.Context,
+		Client:             params.HTTPClient,
+	}
+	for _, opt := range opts {
+		opt(op)
+	}
+
+	result, err := a.transport.Submit(op)
+	if err != nil {
+		return nil, err
+	}
+	success, ok := result.(*HealthOK)
+	if ok {
+		return success, nil
+	}
+	// unexpected success response
+	// safeguard: normally, absent a default response, unknown success responses return an error above: so this is a codegen issue
+	msg := fmt.Sprintf("unexpected success response for health: API contract not enforced by server. Client expected to get an error, but got: %T", result)
 	panic(msg)
 }
 

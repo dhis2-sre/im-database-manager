05fa45ee7ea76fca7a8bcafd3a991d63584bcd00
sj <sebastianthegreatful@gmail.com>
2022-03-03 12:22:44 +0700

    refactor: use curl rather than httpie since curl can show a progress bar for upload/download

--- scripts/download.sh
+++ scripts/download.sh
@@ -4,4 +4,5 @@ set -euo pipefail
 
 ID=$1
 
-$HTTP "$INSTANCE_HOST/databases/$ID/download" "Authorization: Bearer $ACCESS_TOKEN"
+#$HTTP "$INSTANCE_HOST/databases/$ID/download" "Authorization: Bearer $ACCESS_TOKEN"
+curl -H "Authorization: $ACCESS_TOKEN" -L "$INSTANCE_HOST/databases/$ID/download" | cat
--- scripts/upload.sh
+++ scripts/upload.sh
@@ -5,4 +5,5 @@ set -euo pipefail
 ID=$1
 FILE=$2
 
-$HTTP --ignore-stdin --form post "$INSTANCE_HOST/databases/$ID/upload" "database@$FILE" "Authorization: Bearer $ACCESS_TOKEN"
+#$HTTP --ignore-stdin --form post "$INSTANCE_HOST/databases/$ID/upload" "database@$FILE" "Authorization: Bearer $ACCESS_TOKEN"
+curl --fail --progress-bar -H "Authorization: $ACCESS_TOKEN" -F "database=@$FILE" -L "$INSTANCE_HOST/databases/$ID/upload" | cat

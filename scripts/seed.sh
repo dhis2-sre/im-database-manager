8c04b8b445160d872eae68edba1208c04dade8b8
sj <sebastianthegreatful@gmail.com>
2022-05-05 11:04:56 +0200

    chore: implement seed script

---
+++ scripts/seed.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+set -x
+
+GROUP_NAME=whoami
+
+function createDatabase() {
+  echo "Login..."
+  export ACCESS_TOKEN="" && eval $(./login.sh) && echo "$ACCESS_TOKEN"
+
+  echo "Creating database... $1"
+  DATABASE_ID=$(./create.sh "$1" $GROUP_NAME | jq -r '.ID')
+
+  echo "Downloading database... $1"
+  curl -C - "$2" -o "$HOME/Downloads/$1.sql.gz"
+  #curl "$2" -o "$HOME/Downloads/$1.sql.gz"
+
+  echo "Login..."
+  export ACCESS_TOKEN="" && eval $(./login.sh) && echo "$ACCESS_TOKEN"
+
+  echo "Uploading database...$1"
+  ./upload.sh "$DATABASE_ID" "$HOME/Downloads/$1.sql.gz"
+}
+
+
+createDatabase "Sierra Leone - 2.36.0" https://databases.dhis2.org/sierra-leone/2.36.0/dhis2-db-sierra-leone.sql.gz
+
+createDatabase "Sierra Leone - 2.36.1" https://databases.dhis2.org/sierra-leone/2.36.1/dhis2-db-sierra-leone.sql.gz
+createDatabase "Sierra Leone - 2.36.2" https://databases.dhis2.org/sierra-leone/2.36.2/dhis2-db-sierra-leone.sql.gz
+createDatabase "Sierra Leone - 2.36.3" https://databases.dhis2.org/sierra-leone/2.36.3/dhis2-db-sierra-leone.sql.gz
+createDatabase "Sierra Leone - 2.36.4" https://databases.dhis2.org/sierra-leone/2.36.4/dhis2-db-sierra-leone.sql.gz
+createDatabase "Sierra Leone - 2.36.5" https://databases.dhis2.org/sierra-leone/2.36.5/dhis2-db-sierra-leone.sql.gz
+
+createDatabase "Sierra Leone - 2.36.6" https://databases.dhis2.org/sierra-leone/2.36.6/dhis2-db-sierra-leone.sql.gz
+
+createDatabase "Sierra Leone - 2.37.0" https://databases.dhis2.org/sierra-leone/2.37.0/dhis2-db-sierra-leone.sql.gz
+createDatabase "Sierra Leone - 2.37.1" https://databases.dhis2.org/sierra-leone/2.37.1/dhis2-db-sierra-leone.sql.gz
+createDatabase "Sierra Leone - 2.37.2" https://databases.dhis2.org/sierra-leone/2.37.2/dhis2-db-sierra-leone.sql.gz
+createDatabase "Sierra Leone - 2.37.3" https://databases.dhis2.org/sierra-leone/2.37.3/dhis2-db-sierra-leone.sql.gz
+createDatabase "Sierra Leone - 2.37.4" https://databases.dhis2.org/sierra-leone/2.37.4/dhis2-db-sierra-leone.sql.gz
+createDatabase "Sierra Leone - 2.37.5" https://databases.dhis2.org/sierra-leone/2.37.5/dhis2-db-sierra-leone.sql.gz
+createDatabase "Sierra Leone - 2.37.6" https://databases.dhis2.org/sierra-leone/2.37.6/dhis2-db-sierra-leone.sql.gz
+
+./list.sh | jq '.[].Databases[] | .ID, .Name, .Url'

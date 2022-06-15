588a3677d5ec701ffe51bc5da740fefc426fd49c
teleivo <teleivo@users.noreply.github.com>
2022-06-15 06:47:59 +0200

    chore: only exit in main
    we should only call os.Exit in main.
    deferred functions are not called when os.Exit is called. In order
    to always exit cleanly, easy to understand the entry and exit of our
    apps we should only call os.Exit (log.Fatal) in main

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -2,7 +2,6 @@ package config
 
 import (
 	"fmt"
-	"log"
 	"os"
 	"strconv"
 )
@@ -17,39 +16,51 @@ type Config struct {
 	Bucket         string
 }
 
-func New() Config {
-	return Config{
-		BasePath: requireEnv("BASE_PATH"),
-		UserService: service{
-			Host:     requireEnv("USER_SERVICE_HOST"),
-			BasePath: requireEnv("USER_SERVICE_BASE_PATH"),
-		},
-		JobService: service{
-			Host:     requireEnv("JOB_SERVICE_HOST"),
-			BasePath: requireEnv("JOB_SERVICE_BASE_PATH"),
-		},
-		Postgresql: postgresql{
-			Host:         requireEnv("DATABASE_HOST"),
-			Port:         requireEnvAsInt("DATABASE_PORT"),
-			Username:     requireEnv("DATABASE_USERNAME"),
-			Password:     requireEnv("DATABASE_PASSWORD"),
-			DatabaseName: requireEnv("DATABASE_NAME"),
-		},
-		RabbitMqURL: rabbitmq{
-			Host:     requireEnv("RABBITMQ_HOST"),
-			Port:     requireEnvAsInt("RABBITMQ_PORT"),
-			Username: requireEnv("RABBITMQ_USERNAME"),
-			Password: requireEnv("RABBITMQ_PASSWORD"),
-		},
-		Authentication: Authentication{
-			Jwks: Jwks{
-				Host:                   requireEnv("JWKS_HOST"),
-				Index:                  requireEnvAsInt("JWKS_INDEX"),
-				MinimumRefreshInterval: requireEnvAsInt("JWKS_MINIMUM_REFRESH_INTERVAL"),
-			},
-		},
-		Bucket: requireEnv("S3_BUCKET"),
+func New() (Config, error) {
+	basePath, err := requireEnv("BASE_PATH")
+	if err != nil {
+		return Config{}, err
 	}
+
+	usrSvc, err := newUserService()
+	if err != nil {
+		return Config{}, err
+	}
+
+	jobSvc, err := newJobService()
+	if err != nil {
+		return Config{}, err
+	}
+
+	pg, err := newPostgresql()
+	if err != nil {
+		return Config{}, err
+	}
+
+	rb, err := newRabbitMQ()
+	if err != nil {
+		return Config{}, err
+	}
+
+	auth, err := newAuthentication()
+	if err != nil {
+		return Config{}, err
+	}
+
+	bucket, err := requireEnv("S3_BUCKET")
+	if err != nil {
+		return Config{}, err
+	}
+
+	return Config{
+		BasePath:       basePath,
+		UserService:    usrSvc,
+		JobService:     jobSvc,
+		Postgresql:     pg,
+		RabbitMqURL:    rb,
+		Authentication: auth,
+		Bucket:         bucket,
+	}, nil
 }
 
 type service struct {
@@ -57,6 +68,38 @@ type service struct {
 	BasePath string
 	Username string
 	Password string
+}
+
+func newUserService() (service, error) {
+	host, err := requireEnv("USER_SERVICE_HOST")
+	if err != nil {
+		return service{}, err
+	}
+	basePath, err := requireEnv("USER_SERVICE_BASE_PATH")
+	if err != nil {
+		return service{}, err
+	}
+
+	return service{
+		Host:     host,
+		BasePath: basePath,
+	}, nil
+}
+
+func newJobService() (service, error) {
+	host, err := requireEnv("JOB_SERVICE_HOST")
+	if err != nil {
+		return service{}, err
+	}
+	basePath, err := requireEnv("JOB_SERVICE_BASE_PATH")
+	if err != nil {
+		return service{}, err
+	}
+
+	return service{
+		Host:     host,
+		BasePath: basePath,
+	}, nil
 }
 
 type postgresql struct {
@@ -67,11 +110,68 @@ type postgresql struct {
 	DatabaseName string
 }
 
+func newPostgresql() (postgresql, error) {
+	host, err := requireEnv("DATABASE_HOST")
+	if err != nil {
+		return postgresql{}, err
+	}
+	port, err := requireEnvAsInt("DATABASE_PORT")
+	if err != nil {
+		return postgresql{}, err
+	}
+	usrname, err := requireEnv("DATABASE_USERNAME")
+	if err != nil {
+		return postgresql{}, err
+	}
+	pw, err := requireEnv("DATABASE_PASSWORD")
+	if err != nil {
+		return postgresql{}, err
+	}
+	name, err := requireEnv("DATABASE_NAME")
+	if err != nil {
+		return postgresql{}, err
+	}
+
+	return postgresql{
+		Host:         host,
+		Port:         port,
+		Username:     usrname,
+		Password:     pw,
+		DatabaseName: name,
+	}, nil
+}
+
 type rabbitmq struct {
 	Host     string
 	Port     int
 	Username string
 	Password string
+}
+
+func newRabbitMQ() (rabbitmq, error) {
+	host, err := requireEnv("RABBITMQ_HOST")
+	if err != nil {
+		return rabbitmq{}, err
+	}
+	port, err := requireEnvAsInt("RABBITMQ_PORT")
+	if err != nil {
+		return rabbitmq{}, err
+	}
+	username, err := requireEnv("RABBITMQ_USERNAME")
+	if err != nil {
+		return rabbitmq{}, err
+	}
+	pw, err := requireEnv("RABBITMQ_PASSWORD")
+	if err != nil {
+		return rabbitmq{}, err
+	}
+
+	return rabbitmq{
+		Host:     host,
+		Port:     port,
+		Username: username,
+		Password: pw,
+	}, nil
 }
 
 func (r rabbitmq) GetUrl() string {
@@ -82,25 +182,52 @@ type Authentication struct {
 	Jwks Jwks
 }
 
+func newAuthentication() (Authentication, error) {
+	host, err := requireEnv("JWKS_HOST")
+	if err != nil {
+		return Authentication{}, err
+	}
+	index, err := requireEnvAsInt("JWKS_INDEX")
+	if err != nil {
+		return Authentication{}, err
+	}
+	refreshInterval, err := requireEnvAsInt("JWKS_MINIMUM_REFRESH_INTERVAL")
+	if err != nil {
+		return Authentication{}, err
+	}
+
+	return Authentication{
+		Jwks: Jwks{
+			Host:                   host,
+			Index:                  index,
+			MinimumRefreshInterval: refreshInterval,
+		},
+	}, nil
+}
+
 type Jwks struct {
 	Host                   string
 	Index                  int
 	MinimumRefreshInterval int
 }
 
-func requireEnv(key string) string {
+func requireEnv(key string) (string, error) {
 	value, exists := os.LookupEnv(key)
 	if !exists {
-		log.Fatalf("Can't find environment variable: %s\n", key)
+		return "", fmt.Errorf("can't find environment variable: %s", key)
 	}
-	return value
+	return value, nil
 }
 
-func requireEnvAsInt(key string) int {
-	valueStr := requireEnv(key)
+func requireEnvAsInt(key string) (int, error) {
+	valueStr, err := requireEnv(key)
+	if err != nil {
+		return 0, err
+	}
+
 	value, err := strconv.Atoi(valueStr)
 	if err != nil {
-		log.Fatalf("Can't parse value as integer: %s", err.Error())
+		return 0, fmt.Errorf("can't parse value as integer: %v", err)
 	}
-	return value
+	return value, nil
 }

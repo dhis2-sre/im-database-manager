b41dd33c823bc36e25f3df47b0042c95c7e39497
TonsV2 <sebastianthegreatful@gmail.com>
2022-08-05 12:49:54 +0700

    refactor: remove comment

--- scripts/download.sh
+++ scripts/download.sh
@@ -4,5 +4,4 @@ set -euo pipefail
 
 DATABASE=$1
 
-#$HTTP "$INSTANCE_HOST/databases/$DATABASE/download" "Authorization: Bearer $ACCESS_TOKEN"
 curl -H "Authorization: $ACCESS_TOKEN" -L "$INSTANCE_HOST/databases/$DATABASE/download" | cat

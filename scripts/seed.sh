f7aa0189a682169a07162be163ba06dfda3ae469
TonsV2 <sebastianthegreatful@gmail.com>
2022-09-06 15:05:37 +0700

    fix: "jq: error (at <stdin>:0): Cannot iterate over null (null)"

--- scripts/seed.sh
+++ scripts/seed.sh
@@ -21,4 +21,4 @@ createDatabase "Sierra Leone - 2.38.1" https://databases.dhis2.org/sierra-leone/
 createDatabase "Sierra Leone - 2.37.7" https://databases.dhis2.org/sierra-leone/2.37.6/dhis2-db-sierra-leone.sql.gz
 createDatabase "Sierra Leone - 2.36.11" https://databases.dhis2.org/sierra-leone/2.36.0/dhis2-db-sierra-leone.sql.gz
 
-./list.sh | jq '.[].Databases[] | .ID, .Name, .Url'
+./list.sh | jq '.[].Databases[]? | .ID, .Name, .Url'

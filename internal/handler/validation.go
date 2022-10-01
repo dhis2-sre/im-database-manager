9ccbf1b9c9481350a373fe6a668584c29880bb71
TonsV2 <sebastianthegreatful@gmail.com>
2022-10-01 15:27:46 +0700

    refactor: extract function

--- internal/handler/validation.go
+++ internal/handler/validation.go
@@ -7,19 +7,21 @@ import (
 	"strings"
 )
 
+func oneOf(fl validator.FieldLevel) bool {
+	matches := strings.Split(fl.Param(), " ")
+	value := fl.Field().String()
+	for _, match := range matches {
+		if match == value {
+			return true
+		}
+	}
+	return false
+}
+
 // RegisterValidation Inspiration: https://blog.logrocket.com/gin-binding-in-go-a-tutorial-with-examples/
 func RegisterValidation() error {
 	if v, ok := binding.Validator.Engine().(*validator.Validate); ok {
-		return v.RegisterValidation("oneOf", func(fl validator.FieldLevel) bool {
-			matches := strings.Split(fl.Param(), " ")
-			value := fl.Field().String()
-			for _, match := range matches {
-				if match == value {
-					return true
-				}
-			}
-			return false
-		})
+		return v.RegisterValidation("oneOf", oneOf)
 	}
 	return fmt.Errorf("error getting validation engine")
 }

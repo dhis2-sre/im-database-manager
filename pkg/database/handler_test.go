186f68c27c88375da02e01bbabddc869f8311fb4
TonsV2 <sebastianthegreatful@gmail.com>
2023-01-17 13:20:53 +0700

    chore: assert error message

--- pkg/database/handler_test.go
+++ pkg/database/handler_test.go
@@ -84,6 +84,7 @@ func TestHandler_List_RepositoryError(t *testing.T) {
 	assert.Empty(t, w.Body.Bytes())
 	assert.NotEmpty(t, c.Errors)
 	assert.Len(t, c.Errors, 1)
+	assert.Equal(t, errorMessage, c.Errors[0].Error())
 
 	repository.AssertExpectations(t)
 }

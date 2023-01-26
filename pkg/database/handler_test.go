b9aa0f67859dab335d3d8c0560902f67826b64bf
TonsV2 <sebastianthegreatful@gmail.com>
2023-01-26 13:47:52 +0800

    chore: force local time

--- pkg/database/handler_test.go
+++ pkg/database/handler_test.go
@@ -32,7 +32,7 @@ func TestHandler_CreateExternalDownload(t *testing.T) {
 	repository.
 		On("PurgeExternalDownload").
 		Return(nil)
-	expiration := time.Now().Add(time.Duration(1) * time.Hour).Round(time.Duration(1))
+	expiration := time.Now().Add(time.Duration(1) * time.Hour).Round(time.Duration(1)).Local()
 	externalDownload := model.ExternalDownload{
 		UUID:       uuid.UUID{},
 		Expiration: expiration,

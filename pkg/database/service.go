abcf5fd27027a325e0cd099572f0a330a6faf43b
TonsV2 <sebastianthegreatful@gmail.com>
2023-02-23 10:31:21 +0800

    chore: use path.Join

--- pkg/database/service.go
+++ pkg/database/service.go
@@ -9,6 +9,7 @@ import (
 	"log"
 	"net/url"
 	"os"
+	"path"
 	"strconv"
 	"strings"
 	"time"
@@ -224,7 +225,7 @@ func (s service) FindExternalDownload(uuid uuid.UUID) (model.ExternalDownload, e
 
 func (s service) SaveAs(token string, database *model.Database, instance *instanceModels.Instance, stack *instanceModels.Stack, newName string, format string) (*model.Database, error) {
 	// TODO: Add to config
-	dumpPath := "/mnt/data/"
+	dumpPath := "/mnt/data"
 
 	group, err := s.userClient.FindGroupByName(token, instance.GroupName)
 	if err != nil {
@@ -299,7 +300,7 @@ func (s service) SaveAs(token string, database *model.Database, instance *instan
 			return
 		}
 
-		dumpFile := dumpPath + dumpExec.File
+		dumpFile := path.Join(dumpPath, dumpExec.File)
 		file, err := os.Open(dumpFile) // #nosec
 		if err != nil {
 			logError(err)
@@ -308,7 +309,7 @@ func (s service) SaveAs(token string, database *model.Database, instance *instan
 		defer removeTempFile(file)
 
 		if format == "plain" {
-			gzFileName := dumpPath + fileId + ".gz"
+			gzFileName := path.Join(dumpPath, fileId+".gz")
 			file, err = gz(gzFileName, database, file)
 			if err != nil {
 				logError(err)
